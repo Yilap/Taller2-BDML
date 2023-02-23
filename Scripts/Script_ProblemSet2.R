859cec2495b4cf5db1771fb73dc9ef88bc212dc8
Betina Cortés <b.cortes@uniandes.edu.co>
2023-02-23 00:58:28 -0500

    Update Script_ProblemSet2.R

--- Scripts/Script_ProblemSet2.R
+++ Scripts/Script_ProblemSet2.R
@@ -15,4 +15,15 @@ p_load("tidyverse","rvest","rio","skimr","caret","ggplot2","stargazer","readr")
 
 # Importing Dataset -------------------------------------------------------
 
+# Se importan los 4 archivos a usar
+
 test_hogares <- read_csv("Downloads/uniandes-bdml-20231-ps2/test_hogares.csv")
+train_hogares <- read_csv("Downloads/uniandes-bdml-20231-ps2/train_hogares.csv")
+test_personas <- read_csv("Downloads/uniandes-bdml-20231-ps2/test_personas.csv")
+train_personas <- read_csv("Downloads/uniandes-bdml-20231-ps2/train_personas.csv")
+
+# Los datos se guardan como un archivo binario R (rds) usando saveRDS()
+# para hacer más eficiente la carga cuando sea necesario
+
+saveRDS(GEIH, file = "GEIH1.rds")
+GEIH<-readRDS("GEIH1.Rds")
